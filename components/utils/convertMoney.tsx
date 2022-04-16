4f3c47d858db65594e9d2c8a7fe71d6e8a91d98d
deadend <deadend@pop-os.localdomain>
2022-04-15 22:39:35 -0400

    format

--- components/utils/convertMoney.tsx
+++ components/utils/convertMoney.tsx
@@ -2,7 +2,7 @@ import { FormatMoney } from 'format-money-js';
 
 export const convertMoney = (value:number) => {
 
-	const fm = new FormatMoney({ symbol: '$',decimals: 2 });
-	const values:string = fm.from(value).toString();
+	const fm = new FormatMoney();
+	const values:string = fm.from(value,{ symbol: '$',decimals: 2 }).toString();
 	return values;
 };
