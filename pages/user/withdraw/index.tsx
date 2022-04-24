dcff24006235729f813beef9c4a19271cbf516c9
isaquerr25 <isaqueribeiro964@gmail.com>
2022-04-24 17:50:25 -0400

    asdasd

--- pages/user/withdraw/index.tsx
+++ pages/user/withdraw/index.tsx
@@ -8,11 +8,12 @@ import { NavLogin } from '../../../components/utils/navigateLogin';
 import { ForexRow } from '../../../components/pageComponents/user/forextoRow';
 import { BodyWithdraw } from '../../../components/pageComponents/user/withdraw/body';
 import Layout from '../../../components/utils/layout';
-
-
+import { useEffect, useRef, useState } from 'react';
 
 
 const Withdraw = () =>{
+	const size = useWindowSize();
+
 	return(
 		<Layout title={'Requests'} navType={'user'} displayFooter={false}>
 			<Box cursor={'none'} pointerEvents='none' className='forexShowcss' w={'full'}>
@@ -20,7 +21,7 @@ const Withdraw = () =>{
 			</Box>
 			<BodyWithdraw/>
 			<Box cursor={'none'} pointerEvents='none' className='forexShowcss' w={'full'}>
-				<MiniChart colorTheme="dark" isTransparent width="100%"></MiniChart>
+				<MiniChart colorTheme="dark" isTransparent width={size.width} ></MiniChart>
 			</Box>
 		</Layout>
 
@@ -28,3 +29,36 @@ const Withdraw = () =>{
 };
 
 export default Withdraw;
+
+
+
+function useWindowSize() {
+	// Initialize state with undefined width/height so server and client renders match
+	// Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
+	const [windowSize, setWindowSize] = useState<any>({
+	  width: undefined,
+	  height: undefined,
+	});
+  
+	useEffect(() => {
+	  // Handler to call on window resize
+	  function handleResize() {
+		// Set window width/height to state
+			setWindowSize({
+		  width: window.innerWidth,
+		  height: window.innerHeight,
+			});
+	  }
+  
+	  // Add event listener
+	  window.addEventListener('resize', handleResize);
+  
+	  // Call handler right away so state gets updated with initial window size
+	  handleResize();
+  
+	  // Remove event listener on cleanup
+	  return () => window.removeEventListener('resize', handleResize);
+	}, []); // Empty array ensures that effect is only run on mount
+  
+	return windowSize;
+}
