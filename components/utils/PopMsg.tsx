92e6f00dfb36e61b310a2ea47ab17c421de1bd04
isaquerr25 <isaqueribeiro964@gmail.com>
2022-04-25 21:35:55 -0400

    pop up color

--- components/utils/PopMsg.tsx
+++ components/utils/PopMsg.tsx
@@ -19,19 +19,24 @@ export function PopMsg({title,msg,display,hide,nameButton}:typePopup) {
 		<>
 			<Modal  onClose={hide} isOpen={display} isCentered>
 				<ModalOverlay />
-				<ModalContent>
+				<ModalContent 
+					borderColor={'teal'}
+					style={{border: '4px solid teal'}} 
+					bg={'black'}
+					color={'teal'}
+				>
 					<ModalHeader>{title}</ModalHeader>
 					<ModalCloseButton />
 					<ModalBody display={'flex'} flexDirection='column' minH={'100px'} gap={5} width='100%' justifyContent='center' alignItems={'center'}>
 						{title==='Success' && <Icon alignItems={'center'}  boxSize={'20'} as={GrValidate} color='green' />}
 						{title==='Payment' && <Icon alignItems={'center'}  boxSize={'20'} as={BsBasket} color='teal.500' />}
 						{(title!='Success' && title!='Payment')  && <Icon alignItems={'center'}  boxSize={'20'} as={VscError} color='red' />}
-						<Text>
+						<Text fontWeight={'bold'}>
 							{msg}
 						</Text>
 					</ModalBody>
 					<ModalFooter  display={'flex'} flexDirection='column'  justifyContent='center' alignItems={'center'}>
-						<Button onClick={hide}>{nameButton ?? 'Close'}</Button>
+						<Button   colorScheme='teal' variant='outline' onClick={hide}>{nameButton ?? 'Close'}</Button>
 					</ModalFooter>
 				</ModalContent>
 			</Modal>
